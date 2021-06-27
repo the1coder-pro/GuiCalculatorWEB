371cb5f2f7199f9228628ab17b2e0714397032a2
MutawaPro <42838648+the1coder-pro@users.noreply.github.com>
2021-06-27 22:59:13 +0000

    Add some addition method

--- engine.js
+++ engine.js
@@ -1,18 +1,43 @@
 // console.log(document.querySelector("#calc1"));
 // const a = 1;
 // console.log(resultInput.defaultValue);
-
+var numbers = [];
+var intgetNumbers = [];
+var result;
 
 function addValue(btn) {
-
+  
     var resultInput = document.getElementById('calc1');
+   
+    if (btn.value == "+"){
+      numbers.push(resultInput.value);
 
-    console.log(resultInput);
-    // resultInput.value += btn;
-    console.log(btn.value);
-    resultInput.value += btn.value;
+    } else if (btn.value == "="){
+      numbers.push(resultInput.value);
+      result = +numbers[0] + +numbers[1];
+      console.log(result);
+      resultInput.value += result;
+    }
 
+     resultInput.value += btn.value;
+     if (result != undefined) {
+       resultInput.value = result;
+     }
+    //  if (resultInput != NaN){
+
+    //  }
+    //  resultInput.value += resultInput;
+    // console.log(resultInput.value.split('+'));
+    
+
+    
+
+
+    // console.log(btn.value);
 }
+
+
+
 function deleteChr() {
 
     var resultInput = document.getElementById('calc1');
@@ -22,16 +47,23 @@ function deleteChr() {
 
 }
 
+function calculate(){
+        var resultInput = document.getElementById('calc1');
 
-function calculateInput() {
-  var resultInput = document.getElementById('calc1');
+      //  if(typeof document.getElementById("calc1").value == "+")
+      //  {
+      //     var num = splitedValue[0];
+      //  }      
 
-  var resultSplited = resultInput.value.split('');
-  console.log(resultSplited);
-  if (resultSplitd.includes())
+      var numbers = [];
+
+      var splitedValue = resultInput.value.split('+');
+      console.log(splitedValue);
+      for (number in splitedValue) {
+        numbers.push(parseInt(number));
+      }
+     
 }
 
-// function myFunction() {
-//     );
-// }
+
 
