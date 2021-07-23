819062268642d8bddffbdead346526c73b80782c
MutawaPro <42838648+the1coder-pro@users.noreply.github.com>
2021-07-23 05:15:55 +0000

    Improved design and functionality.

--- engine.js
+++ engine.js
@@ -6,47 +6,42 @@ var intgetNumbers = [];
 var result;
 
 function addValue(btn) {
-  
-    var resultInput = document.getElementById('calc1');
-     if (btn.value == "="){
-       resultInput.value = eval(resultInput.value);
-     }else {
-       resultInput.value += btn.value;
-     }
-    
+  var resultInput = document.getElementById("calc1");
+  if (btn.value == "=" && resultInput.value.indexOf("+") == -1) {
+    resultInput.value = parseInt(resultInput.value) + parseInt(resultInput.value);
+  } else if (btn.value == "=") {
+    resultInput.value = eval(resultInput.value);
+  } else {
+    resultInput.value += btn.value;
+  }
 }
 
 function clearInput() {
-  document.getElementById('calc1').value = "";
+  document.getElementById("calc1").value = "";
 }
-
 
 function deleteChr() {
+  var resultInput = document.getElementById("calc1");
 
-    var resultInput = document.getElementById('calc1');
-
-
-    resultInput.value = resultInput.value.substring(0, resultInput.value.length - 1);
-
+  resultInput.value = resultInput.value.substring(
+    0,
+    resultInput.value.length - 1
+  );
 }
 
-function calculate(){
-        var resultInput = document.getElementById('calc1');
+function calculate() {
+  var resultInput = document.getElementById("calc1");
 
-      //  if(typeof document.getElementById("calc1").value == "+")
-      //  {
-      //     var num = splitedValue[0];
-      //  }      
+  //  if(typeof document.getElementById("calc1").value == "+")
+  //  {
+  //     var num = splitedValue[0];
+  //  }
 
-      var numbers = [];
+  var numbers = [];
 
-      var splitedValue = resultInput.value.split('+');
-      console.log(splitedValue);
-      for (number in splitedValue) {
-        numbers.push(parseInt(number));
-      }
-     
+  var splitedValue = resultInput.value.split("+");
+  console.log(splitedValue);
+  for (var number in splitedValue) {
+    numbers.push(parseInt(number));
+  }
 }
-
-
-
